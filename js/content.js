fc817bb83e45a312fa47754fc5a80c753978644c
wmi1105 <dkcla7788@gmail.com>
2020-05-01 15:09:22 +0900

    반응형
    반응형 레이아웃 적용

--- js/content.js
+++ js/content.js
@@ -14,32 +14,32 @@ $(document).ready(function(){
 
     })
 
-    $(window).resize(projectFieldResize);
-    projectFieldResize();
-    function projectFieldResize(){
-        var windowWidth = $(window).width();
+    // $(window).resize(projectFieldResize);
+    // projectFieldResize();
+    // function projectFieldResize(){
+    //     var windowWidth = $(window).width();
         
 
-        if(windowWidth > 991){
-            var boxHeight = $('.item-list').height();
-            // $(".item-box").css('height',boxHeight);
-        }
+    //     if(windowWidth > 991){
+    //         var boxHeight = $('.item-list').height();
+    //         // $(".item-box").css('height',boxHeight);
+    //     }
 
-        if(windowWidth <= 991){
-            var sumMargin = 16+3;
-            var fstLine = $(".item-list li:nth-child(3)").height()+sumMargin;
-            var sndLine =  $(".item-list li:nth-child(4)").height()+sumMargin;
-            var trdLine =  $(".item-list li:nth-child(5)").height()+sumMargin;
+    //     if(windowWidth <= 991){
+    //         var sumMargin = 16+3;
+    //         var fstLine = $(".item-list li:nth-child(3)").height()+sumMargin;
+    //         var sndLine =  $(".item-list li:nth-child(4)").height()+sumMargin;
+    //         var trdLine =  $(".item-list li:nth-child(5)").height()+sumMargin;
     
-            var allHeight = $(".item-list").height()+$(".item-info").height()+20;   ///+padding
+    //         var allHeight = $(".item-list").height()+$(".item-info").height()+20;   ///+padding
             
-            $(".item-list li:nth-child(-n+3)").css('height',fstLine);   //li 1~3
-            $(".item-list li:nth-child(n+4):nth-child(-n+6)").css('height',sndLine); // li 4~6
-            $(".item-list li:nth-child(n+7)").css('height',trdLine);    //li 7~9
+    //         $(".item-list li:nth-child(-n+3)").css('height',fstLine);   //li 1~3
+    //         $(".item-list li:nth-child(n+4):nth-child(-n+6)").css('height',sndLine); // li 4~6
+    //         $(".item-list li:nth-child(n+7)").css('height',trdLine);    //li 7~9
 
-            // $("#project .item-box").css('height', allHeight);
-        }
-    }
+    //         // $("#project .item-box").css('height', allHeight);
+    //     }
+    // }
 
 
 })
